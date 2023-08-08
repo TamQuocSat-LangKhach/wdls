2ea06619e3ba02af80d002eb53550a5653456ee6
RalphR <1715333874@qq.com>
2023-08-08 16:34:13 +0800

    虚实卡牌

--- wd_xushi_cards.lua
+++ wd_xushi_cards.lua
@@ -110,7 +110,9 @@ local wdRunTrigger = fk.CreateTriggerSkill{
   on_use = function(self, event, target, player, data)
     local use = self.cost_data
     player.room:useCard(use)
-    table.insertIfNeed(data.nullifiedTargets, player.id)
+    if not table.contains(use.nullifiedTargets, player.id) then  --FIXME: 还有取消等情况！
+      table.insertIfNeed(data.nullifiedTargets, player.id)
+    end
   end,
 }
 local wdRunSkill = fk.CreateActiveSkill{
@@ -131,6 +133,11 @@ local wdRunSkill = fk.CreateActiveSkill{
     if not target.dead and not target:isKongcheng() then
       target:showCards(target:getCardIds("h"))
     end
+    --[[local event = room.logic:getCurrentEvent():findParent(GameEvent.UseCard)
+    if event then
+      local data = event.data[1]
+      table.insertIfNeed(data.nullifiedTargets, target.id)
+    end]]--FIXME: 正确插入了无效目标但未生效，why？
   end
 }
 local wd_run = fk.CreateBasicCard{
@@ -213,22 +220,78 @@ Fk:loadTranslationTable{
   ["#wd_rice-use"] = "你可以使用【粮】，摸一张牌且本回合手牌上限无限",
 }
 
+local wdGoldTrigger = fk.CreateTriggerSkill{
+  name = "wd_gold_trigger",
+  global = true,
+  mute = true,
+  priority = 0,
+  events = {fk.DamageInflicted},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and data.from and data.from ~= player and not data.from.dead and
+      table.find(player:getCardIds("h"), function(id) return Fk:getCardById(id).trueName == "wd_gold" end)
+  end,
+  on_cost = function(self, event, target, player, data)
+    local use = player.room:askForUseCard(player, "wd_gold", nil, "#wd_gold-use::"..data.from.id, true)
+    if use then
+      self.cost_data = use
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local use = self.cost_data
+    use.tos = {{data.from.id}}
+    player.room:useCard(use)
+  end,
+}
+local wdGoldSkill = fk.CreateActiveSkill{
+  name = "wd_gold_skill",
+  can_use = function()
+    return false
+  end,
+  on_effect = function(self, room, effect)
+    local target = room:getPlayerById(effect.to)
+    if target and not target.dead and not effect.card:isVirtual() or #effect.card.subcards > 0 then
+      room:moveCards{
+        ids = room:getSubcardsByRule(effect.card, { Card.Processing }),
+        to = target.id,
+        toArea = Card.PlayerHand,
+        moveReason = fk.ReasonGive,
+        skillName = self.name,
+        proposer = effect.from,
+      }
+    end
+    local event = room.logic:getCurrentEvent():findParent(GameEvent.Damage)
+    if event then
+      event:shutdown()
+    end
+  end
+}
+local wd_gold = fk.CreateBasicCard{
+  name = "wd_gold",
+  skill = wdGoldSkill,
+}
+Fk:addSkill(wdGoldTrigger)
+extension:addCards{
+  wd_gold:clone(Card.Heart, 2),
+  wd_gold:clone(Card.Diamond, 2),
+}
 Fk:loadTranslationTable{
   ["wd_gold"] = "金",
-  ["wd_gold_skill"] = "金",--♥2，♦2
+  ["wd_gold_skill"] = "金",
 	[":wd_gold"] = "基本牌<br/><b>时机</b>：受到其他角色造成的伤害时<br/><b>目标</b>：伤害来源<br/><b>效果</b>：目标角色获得此牌，防止此伤害。",
+  ["#wd_gold-use"] = "你可以使用【金】，令伤害来源 %dest 获得【金】，防止你受到的伤害",
 }
 
 extension:addCards{
   Fk:cloneCard("iron_chain", Card.Spade, 4),
 }
 
-Fk:loadTranslationTable{
+--[[Fk:loadTranslationTable{
   ["wd_gold"] = "草船借箭",
   ["wd_gold_skill"] = "草船借箭",--♠K，♦K
 	[":wd_gold"] = "锦囊牌<br/><b>时机</b>：出牌阶段<br/><b>目标</b>：一名有手牌的其他角色<br/><b>效果</b>：目标选择一项：1.将至少一张【杀】交给你，"..
   "若其中有火【杀】，其对你造成1点火属性伤害；2.令你观看其手牌并弃置其中一张牌，若如此做，其手牌于此回合内对所有角色可见。",
-}
+}]]
 
 local wdStopThirstSkill = fk.CreateActiveSkill{
   name = "wd_stop_thirst_skill",
@@ -272,11 +335,11 @@ Fk:loadTranslationTable{
   "然后若目标是体力值最小的角色，其回复1点体力。",
 }
 
-local wdLeOffEnemySkill = fk.CreateActiveSkill{
+local wdLetOffEnemySkill = fk.CreateActiveSkill{
   name = "wd_let_off_enemy_skill",
   target_num = 1,
   mod_target_filter = function(self, to_select, selected, user, card, distance_limited)
-    return to_select ~= Self.id
+    return user ~= to_select
   end,
   target_filter = function(self, to_select)
     return to_select ~= Self.id
@@ -301,17 +364,12 @@ local wdLeOffEnemySkill = fk.CreateActiveSkill{
       room:broadcastPlaySound("./packages/maneuvering/audio/card/".. (player.gender == General.Male and "male/" or "female/") .. "fire_attack")
     end
     room:doIndicate(player.id, {target.id})
-    local cardEffectEvent = effect
-    cardEffectEvent.card = Fk:cloneCard(choice)
-    if not effect.card:isVirtual() then
-      cardEffectEvent.card:addSubcards(effect.card.subcards)
-    end
-    cardEffectEvent.card.skill:onEffect(room, cardEffectEvent)
+    Fk:cloneCard(choice).skill:onEffect(room, effect)
   end,
 }
 local wd_let_off_enemy = fk.CreateTrickCard{
   name = "wd_let_off_enemy",
-  skill = wdLeOffEnemySkill,
+  skill = wdLetOffEnemySkill,
   is_damage_card = true,
 }
 extension:addCards{
@@ -355,7 +413,9 @@ local wdLureInDeepTrigger = fk.CreateTriggerSkill{
     use.toCard = data.card
     use.responseToEvent = data
     player.room:useCard(use)
-    return true
+    if use.responseToEvent.isCancellOut then
+      return true
+    end
   end,
 }
 local wdLureInDeepSkill = fk.CreateActiveSkill{
@@ -410,23 +470,95 @@ Fk:loadTranslationTable{
   ["wd_lure_in_deep"] = "诱敌深入",
   ["wd_lure_in_deep_skill"] = "诱敌深入",
 	[":wd_lure_in_deep"] = "锦囊牌<br/><b>时机</b>：当【杀】对你生效前<br/><b>目标</b>：此【杀】<br/><b>效果</b>：抵消此【杀】对你产生的效果，"..
-  "然后目标需重复对你使用【杀】直到以此法使用的【杀】对你造成伤害，否则你对其造成1点伤害。",
+  "然后此【杀】使用者需重复对你使用【杀】直到以此法使用的【杀】对你造成伤害，否则你对其造成1点伤害。",
   ["#wd_lure_in_deep-use"] = "%src 对你使用%arg，你可以使用【诱敌深入】",
   ["#wd_lure_in_deep-slash"] = "诱敌深入：请继续对 %src 使用【杀】直到对其造成伤害，否则其对你造成1点伤害",
 }
 
-Fk:loadTranslationTable{
+--[[Fk:loadTranslationTable{
   ["wd_gold"] = "调兵遣将",
   ["wd_gold_skill"] = "调兵遣将",--♥4，♦4
 	[":wd_gold"] = "锦囊牌<br/><b>时机</b>：出牌阶段<br/><b>目标</b>：所有角色<br/><b>效果</b>：亮出牌堆顶的X张牌（X为角色数的一半，向下取整）。"..
   "目标角色依次可以用一张手牌替换其中一张牌。结算结束时，你将其中任意张牌以任意顺序置于牌堆顶。",
-}
+}]]
 
+local wdDrowningSkill = fk.CreateActiveSkill{
+  name = "wd_drowning_skill",
+  can_use = Util.AoeCanUse,
+  on_use = Util.AoeOnUse,
+  mod_target_filter = function(self, to_select, selected, user, card, distance_limited)
+    return user ~= to_select
+  end,
+  on_effect = function(self, room, effect)
+    local player = room:getPlayerById(effect.from)
+    local target = room:getPlayerById(effect.to)
+    if not (effect.extra_data and effect.extra_data.wdDrowning) or target.dead then return end
+    if target:hasSkill("#vine_skill") then
+      local skill = Fk.skills["#vine_skill"]
+      skill:use(fk.PreCardEffect, target, target, effect)
+    else
+      if target:isNude() then
+        room:damage({
+          from = player,
+          to = target,
+          damage = 1,
+          card = effect.card,
+          skillName = self.name,
+        })
+      else
+        local card = room:askForDiscard(target, 1, 1, true, self.name, true, ".|.|.|.|.|"..effect.extra_data.wdDrowning,
+          "#wd_drowning-discard:::"..effect.extra_data.wdDrowning)
+        if #card == 0 then
+          room:damage({
+            from = player,
+            to = target,
+            damage = 1,
+            card = effect.card,
+            skillName = self.name,
+          })
+        end
+      end
+    end
+  end
+}
+local wdDrowningAction = fk.CreateTriggerSkill{
+  name = "wd_drowning_action",
+  global = true,
+  priority = 0,
+  events = {fk.BeforeCardUseEffect},
+  can_trigger = function(self, event, target, player, data)
+    return data.card.trueName == 'wd_drowning' and (not data.extra_data or not data.extra_data.wdDrowning)
+  end,
+  on_trigger = function(self, event, target, player, data)
+    local room = player.room
+    local from = room:getPlayerById(data.from)
+    if not from.dead and not from:isKongcheng() then
+      local card = room:askForCard(from, 1, 1, false, "wd_drowning", false, ".", "#wd_drowning-show:::"..data.card:toLogString())
+      data.extra_data = data.extra_data or {}
+      data.extra_data.wdDrowning = Fk:getCardById(card[1]):getTypeString()
+      from:showCards(card)
+    end
+  end,
+}
+Fk:addSkill(wdDrowningAction)
+local wdDrowning = fk.CreateTrickCard{
+  name = "wd_drowning",
+  is_damage_card = true,
+  multiple_targets = true,
+  skill = wdDrowningSkill,
+}
+extension:addCards({
+  wdDrowning:clone(Card.Spade, 1),
+  wdDrowning:clone(Card.Club, 1),
+  wdDrowning:clone(Card.Club, 7),
+})
 Fk:loadTranslationTable{
   ["wd_drowning"] = "水淹七军",
-  ["wd_drowning_skill"] = "水淹七军",--♠A，♣A，♣7
+  ["wd_drowning_skill"] = "水淹七军",
 	[":wd_drowning"] = "锦囊牌<br/><b>时机</b>：出牌阶段<br/><b>目标</b>：所有其他角色<br/><b>效果</b>：你展示一张手牌，目标角色依次选择一项："..
-  "1.弃置一张与此牌类别相同的牌；2.你对其造成1点伤害。",
+  "1.弃置一张与展示牌类别相同的牌；2.你对其造成1点伤害。",
+  ["#wd_drowning-show"] = "请为%arg展示一张手牌",
+  ["#wd_drowning-discard"] = "水淹七军：弃置一张%arg，否则受到1点伤害",
 }
 
 local wdSaveEnergySkill = fk.CreateActiveSkill{
@@ -537,8 +669,100 @@ extension:addCard(wdSevenStarsSword)
 Fk:loadTranslationTable{
   ["wd_seven_stars_sword"] = "七星刀",
   ["#wd_seven_stars_sword_skill"] = "七星刀",
-  [":wd_seven_stars_sword"] = "装备牌·武器<br /><b>攻击范围</b>：2<br/><b>武器技能</b>：锁定技，当你使用【杀】指定目标后，目标抵消此【杀】需"..
+  [":wd_seven_stars_sword"] = "装备牌·武器<br/><b>攻击范围</b>：2<br/><b>武器技能</b>：锁定技，当你使用【杀】指定目标后，目标抵消此【杀】需"..
   "额外使用一张【闪】；你使用【杀】结算后，若此【杀】未对目标角色造成过伤害，其获得你装备区内的【七星刀】。",
+}
+
+local wdSunMoonHalberdSkill = fk.CreateTriggerSkill{
+  name = "#wd_sun_moon_halberd_skill",
+  attached_equip = "wd_sun_moon_halberd",
+  anim_type = "offensive",
+  events = {fk.TargetSpecifying},
+  can_trigger = function(self, event, target, player, data)
+    return target == player and player:hasSkill(self.name) and data.card.trueName == "slash"
+  end,
+  on_cost = function(self, event, target, player, data)
+    local room = player.room
+    local targets = {}
+    for _, p in ipairs(room:getOtherPlayers(player)) do
+      if not player:isProhibited(p, data.card) then
+        table.insertIfNeed(targets, p.id)
+      end
+    end
+    if #targets == 0 then return end
+    local to = room:askForChoosePlayers(player, targets, 1, 1, "#wd_sun_moon_halberd-invoke:::"..data.card:toLogString(), self.name, true)
+    if #to > 0 then
+      self.cost_data = to[1]
+      return true
+    end
+  end,
+  on_use = function(self, event, target, player, data)
+    local room = player.room
+    TargetGroup:pushTargets(data.targetGroup, self.cost_data)
+    if player:getEquipment(Card.SubtypeWeapon) and Fk:getCardById(player:getEquipment(Card.SubtypeWeapon)).name == "wd_sun_moon_halberd" then
+      room:throwCard({player:getEquipment(Card.SubtypeWeapon)}, self.name, player, player)
+    end
+  end,
+}
+local wdSunMoonHalberdTrigger = fk.CreateTriggerSkill{
+  name = "wd_sun_moon_halberd_trigger",
+  mute = true,
+  global = true,
+  priority = 0, -- game rule
+  events = {fk.AfterCardsMove},
+  can_trigger = function(self, event, target, player, data)
+    if table.find(player:getCardIds("he"), function(id) return Fk:getCardById(id).name == "wd_sun_moon_halberd" end) and
+      player:usedSkillTimes(self.name, Player.HistoryTurn) == 0 then
+      for _, move in ipairs(data) do
+        if move.toArea == Card.DiscardPile then
+          for _, info in ipairs(move.moveInfo) do
+            if Fk:getCardById(info.cardId).name == "wd_sun_moon_halberd" and
+              (info.fromArea ~= Card.PlayerEquip or move.moveReason ~= fk.ReasonPutIntoDiscardPile) then
+              return true
+            end
+          end
+        end
+      end
+    end
+  end,
+  on_cost = function(self, event, target, player, data)
+    return true
+  end,
+  on_use = function(self, event, target, player, data)
+    for _, move in ipairs(data) do
+      if move.toArea == Card.DiscardPile then
+        for _, info in ipairs(move.moveInfo) do
+          if Fk:getCardById(info.cardId).name == "wd_sun_moon_halberd" and
+            (info.fromArea ~= Card.PlayerEquip or move.moveReason ~= fk.ReasonPutIntoDiscardPile) then
+            local room = player.room
+            if room:getCardArea(info.cardId) == Card.DiscardPile then
+              room:delay(1500)
+              room:obtainCard(player, info.cardId, true, fk.ReasonPrey)
+              return
+            end
+          end
+        end
+      end
+    end
+  end,
+}
+Fk:addSkill(wdSunMoonHalberdSkill)
+Fk:addSkill(wdSunMoonHalberdTrigger)
+local wd_sun_moon_halberd = fk.CreateWeapon{
+  name = "wd_sun_moon_halberd",
+  attack_range = 2,
+  equip_skill = wdSunMoonHalberdSkill,
+}
+extension:addCards{
+  wd_sun_moon_halberd:clone(Card.Club, 12),
+  wd_sun_moon_halberd:clone(Card.Heart, 13),
+}
+Fk:loadTranslationTable{
+  ["wd_sun_moon_halberd"] = "日月戟",
+  ["#wd_sun_moon_halberd_skill"] = "日月戟",
+  [":wd_sun_moon_halberd"] = "装备牌·武器<br/><b>攻击范围</b>：2<br/><b>武器技能</b>：当你使用【杀】指定目标时，你可以增加一个目标"..
+  "（无距离限制且可重复），然后你弃置装备区里的【日月戟】。每回合限一次，当【日月戟】不因替换进入弃牌堆后，区域内有【日月戟】的角色获得之。",
+  ["#wd_sun_moon_halberd-invoke"] = "日月戟：你可以为此%arg增加一个目标（无距离限制且可重复）",
 }
 
 local wdBreastplateSkill = fk.CreateTriggerSkill{
